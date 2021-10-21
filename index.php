2f890aa2d9861903d503e0ac3c740af1e58ba96c
honganh18 <lehonganhsmsl@gmail.com>
2021-10-21 09:06:31 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Học công nghệ web</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div style="background-color:sandybrown; heigh: 100px">
+       <div class ="logo">
+        <a href=""> 
+            <img src="images/logotruong.png" alt="">
+        </a>
+        </div>
+
+        <div class ="language">
+        <a href=""> 
+        <img src ="images/en.jpg" alt="">  
+        </a>
+        <a href="">
+            <img src="images/vi.jpg" alt="">
+        </a>
+        </div>
+
+    </div>
+    
+ <!--<div style="background-color:beige; height:400px ">
+ 
+ </div>
+ <div style="background-color:red; height:100px ">
+    sdhmfbhmaw
+    <div class=" foot">
+        <div class="right">hhh
+</div>
+<div class=" row">hh
+</div></!-->
+</div>
+</body>
+</html>
